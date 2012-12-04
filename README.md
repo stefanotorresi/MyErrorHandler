cd193a35bab7a5ee02b06413425ae29d574271c1
etienn3 <webdev@stefanotorresi.it>
2012-12-04 10:49:24 +0100

    add composer support

---
+++ README.md
@@ -0,0 +1,17 @@
+MyErrorHandler
+===
+
+MyErrorHandler is a Zend Framework 2 module that introduces custom exceptions and error strategies for XHR requests.
+
+
+Features
+---
+
+-   I18n ready.
+-   Ajax XHR support.
+
+
+Documentation
+---
+
+TO-DO
--- config/autoloader.config.php
+++ config/autoloader.config.php
@@ -3,6 +3,9 @@
 namespace MyErrorHandler;
 
 return array(
+    'Zend\Loader\ClassMapAutoloader' => array(
+        __DIR__ . '/../autoload_classmap.php'
+    ),
     'Zend\Loader\StandardAutoloader' => array(
         'namespaces' => array(
             __NAMESPACE__  => __DIR__ . '/../src/' . __NAMESPACE__,
