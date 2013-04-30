8059b7c5fd27cdd2a878fe34aa2e5d173acd2fc8
Stefano Torresi <webdev@stefanotorresi.it>
2013-04-30 12:55:20 +0200

    moved Module.php to be PSR-0 compliant

--- Module.php
+++ Module.php
@@ -1,58 +1,7 @@
 <?php
-
 /**
- *
- * @author Stefano Torresi <webdeveloper@stefanotorresi.it>
+ * This file is placed here for compatibility with Zendframework 2's ModuleManager.
+ * It allows usage of this module even without composer.
+ * The original Module.php is in 'src/{ModuleNamespace}' in order to respect PSR-0
  */
-
-namespace MyErrorHandler;
-
-use Zend\Mvc\MvcEvent;
-
-class Module
-{
-
-    const RENDERER_HTML = 'html';
-    const RENDERER_JSON = 'json';
-
-    public function onBootstrap(MvcEvent $e)
-    {
-        $events = $e->getApplication()->getEventManager();
-        $sharedEvents = $events->getSharedManager();
-        $services  = $e->getApplication()->getServiceManager();
-        $globalConfig = $services->get('config');
-        $config = $globalConfig[__NAMESPACE__];
-
-        // Attach the new strategy
-        $services->get('MyErrorHandler\Strategy\ExceptionStrategy')->attach($events);
-        $services->get('MyErrorHandler\Strategy\NotFoundStrategy')->attach($events);
-
-        if ($config['log_exceptions']) {
-            // attach logger
-            $sharedEvents->attach('Zend\Mvc\Application', MvcEvent::EVENT_DISPATCH_ERROR,
-                function($e) use ($services) {
-                    $exception = $e->getParam('exception');
-                    while ($exception) {
-                        $services->get('Logger')->crit($exception);
-                        $exception = $exception->getPrevious();
-                    }
-                }
-            );
-        }
-    }
-
-    public function getAutoloaderConfig()
-    {
-        return include __DIR__ . '/config/autoloader.config.php';
-    }
-
-    public function getConfig()
-    {
-        return include __DIR__ . '/config/module.config.php';
-    }
-
-    public function getServiceConfig()
-    {
-        return include __DIR__ . '/config/service.config.php';
-    }
-}
+require_once __DIR__ . '/src/MyErrorHandler/Module.php';
---
+++ src/MyErrorHandler/Module.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ *
+ * @author Stefano Torresi <webdeveloper@stefanotorresi.it>
+ */
+
+namespace MyErrorHandler;
+
+use Zend\Mvc\MvcEvent;
+
+class Module
+{
+
+    const RENDERER_HTML = 'html';
+    const RENDERER_JSON = 'json';
+
+    public function onBootstrap(MvcEvent $e)
+    {
+        $events = $e->getApplication()->getEventManager();
+        $sharedEvents = $events->getSharedManager();
+        $services  = $e->getApplication()->getServiceManager();
+        $globalConfig = $services->get('config');
+        $config = $globalConfig[__NAMESPACE__];
+
+        // Attach the new strategy
+        $services->get('MyErrorHandler\Strategy\ExceptionStrategy')->attach($events);
+        $services->get('MyErrorHandler\Strategy\NotFoundStrategy')->attach($events);
+
+        if ($config['log_exceptions']) {
+            // attach logger
+            $sharedEvents->attach('Zend\Mvc\Application', MvcEvent::EVENT_DISPATCH_ERROR,
+                function($e) use ($services) {
+                    $exception = $e->getParam('exception');
+                    while ($exception) {
+                        $services->get('Logger')->crit($exception);
+                        $exception = $exception->getPrevious();
+                    }
+                }
+            );
+        }
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return include __DIR__ . '/../../config/autoloader.config.php';
+    }
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/../../config/module.config.php';
+    }
+
+    public function getServiceConfig()
+    {
+        return include __DIR__ . '/../../config/service.config.php';
+    }
+}
